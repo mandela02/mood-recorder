d45759bae0e10533c1f53f2e5bb5f2f050e1e526
Tri.bui <tri.bui@monstar-lab.com>
2021-08-01 00:23:07 +0700

    create project

---
+++ mood-recorder/mood-recorder/mood_recorderApp.swift
@@ -0,0 +1,20 @@
+//
+//  mood_recorderApp.swift
+//  mood-recorder
+//
+//  Created by LanNTH on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct mood_recorderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
