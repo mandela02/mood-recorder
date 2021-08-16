35d3328ea2aa5022d4ab15e2f5fc47c60acdc6ab
Tri.bui <tri.bui@monstar-lab.com>
2021-08-16 15:35:17 +0700

    add add more option view

--- mood-recorder/mood-recorder/Application/Scene/Input/CusomOptionView.swift
+++ mood-recorder/mood-recorder/Application/Scene/Input/CusomOptionView.swift
@@ -7,64 +7,6 @@
 
 import SwiftUI
 
-class CustomOptionViewModel: ViewModel {
-    @Published var state: CustomOptionState
-    
-    init(state: CustomOptionState) {
-        self.state = state
-        
-        var allImage = AppImage.allCases
-        allImage.removeLast()
-
-        self.state.images = allImage
-            .chunked(into: 20)
-        self.state.numberOfPage = self.state.images.count
-    }
-    
-    func trigger(_ input: CustomOptionTrigger) {
-        switch input {
-        case .optionTap(index: let index):
-            state.selectedImage = state.images[state.currentPage][index]
-        case .goToPage(page: let page):
-            self.state.currentPage = page
-        case .textChange(text: let text):
-            state.title = text
-        case .loadData:
-            guard let title = state.title,
-                  title != "",
-                  let image = state.selectedImage
-            else {
-                return
-            }
-            let content = ImageAndTitleModel(image: image,
-                                             title: title)
-            state.outputModel = OptionModel(content: content)
-        }
-    }
-    
-    struct CustomOptionState {
-        var images: [[AppImage]] = []
-        var currentPage = 0
-        var numberOfPage = 0
-        
-        var selectedImage: AppImage?
-        var title: String?
-        
-        var outputModel: OptionModel?
-        
-        var isSaveEnable: Bool {
-            return title != nil && title != "" && selectedImage != nil
-        }
-    }
-    
-    enum CustomOptionTrigger {
-        case optionTap(index: Int)
-        case textChange(text: String)
-        case goToPage(page: Int)
-        case loadData
-    }
-}
-
 struct CusomOptionView: View {
     typealias Function = () -> ()
     typealias CallbackFunction = (OptionModel) -> ()
@@ -172,6 +114,7 @@ struct CusomOptionView: View {
                     .foregroundColor(Theme.current.commonColor.textColor)
                     .fontWeight(.regular)
                     .font(.system(size: 10))
+                    .padding(.bottom, 5)
             }
             
             Spacer()
---
+++ mood-recorder/mood-recorder/Application/Scene/Input/CustomOptionViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  CustomOptionViewModel.swift
+//  CustomOptionViewModel
+//
+//  Created by LanNTH on 16/08/2021.
+//
+
+import Foundation
+
+class CustomOptionViewModel: ViewModel {
+    @Published var state: CustomOptionState
+    
+    init(state: CustomOptionState) {
+        self.state = state
+        
+        var allImage = AppImage.allCases
+        allImage.removeLast()
+
+        self.state.images = allImage
+            .chunked(into: 20)
+        self.state.numberOfPage = self.state.images.count
+    }
+    
+    func trigger(_ input: CustomOptionTrigger) {
+        switch input {
+        case .optionTap(index: let index):
+            state.selectedImage = state.images[state.currentPage][index]
+        case .goToPage(page: let page):
+            self.state.currentPage = page
+        case .textChange(text: let text):
+            state.title = text
+        case .loadData:
+            guard let title = state.title,
+                  title != "",
+                  let image = state.selectedImage
+            else {
+                return
+            }
+            let content = ImageAndTitleModel(image: image,
+                                             title: title)
+            state.outputModel = OptionModel(content: content)
+        }
+    }
+    
+    struct CustomOptionState {
+        var images: [[AppImage]] = []
+        var currentPage = 0
+        var numberOfPage = 0
+        
+        var selectedImage: AppImage?
+        var title: String?
+        
+        var outputModel: OptionModel?
+        
+        var isSaveEnable: Bool {
+            return title != nil && title != "" && selectedImage != nil
+        }
+    }
+    
+    enum CustomOptionTrigger {
+        case optionTap(index: Int)
+        case textChange(text: String)
+        case goToPage(page: Int)
+        case loadData
+    }
+}
+
